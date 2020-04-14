ff9053783f37af487fba44d56958a9b05143ab99
Hector Lancaster <56166489+HectorLancaster@users.noreply.github.com>
2020-04-14 16:05:18 +0100

    How to open a file

---
+++ Open File.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 14 09:52:19 2020
+
+@author: Hector
+"""
+with open("\\Users\\Hector\\Desktop\\test.txt","r") as file1:
+    file_stuff = file1.read()
+    print(file_stuff)
+print(file1.closed)
+print(file_stuff)
