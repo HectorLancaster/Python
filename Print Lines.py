b97ba582d60a8cfc401a8ec12f47e9e1c67d71a5
Hector Lancaster <56166489+HectorLancaster@users.noreply.github.com>
2020-04-15 13:06:22 +0100

    Add files via upload

---
+++ Print Lines.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 14 09:52:19 2020
+
+@author: Hector
+ 
+"""
+filename = "C:\\Users\\Hector\\Desktop\\input.txt"
+for line in open(filename):
+    print(line)
