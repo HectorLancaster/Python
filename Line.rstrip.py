06323c4562f6e96525bd7b1277a152773e2e8788
Hector Lancaster <56166489+HectorLancaster@users.noreply.github.com>
2020-04-15 13:08:50 +0100

    Add files via upload

---
+++ Line.rstrip.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 14 09:52:19 2020
+
+@author: Hector
+ 
+"""
+filename = "C:\\Users\\Hector\\Desktop\\input.txt"
+for line in open(filename):
+    line = line.rstrip()
+    print(line)
