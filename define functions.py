4aa93817f03270dcc10be7d1afab2f1b98d5c565
Hector Lancaster <56166489+HectorLancaster@users.noreply.github.com>
2020-04-15 13:20:20 +0100

    Add files via upload

---
+++ define functions.py
@@ -0,0 +1,8 @@
+# define functions, using tuples to return multiple values
+def add_and_sub(a,b):
+    """
+    returns the sum and difference as (sum, difference)
+    """
+    mysum = a + b
+    mydiff = a - b
+    return(mysum, mydiff)
