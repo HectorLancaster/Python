08956e504144ddccf2546963c55707d95cfc4a6d
Hector Lancaster <56166489+HectorLancaster@users.noreply.github.com>
2020-04-16 11:08:07 +0100

    Add files via upload

---
+++ Distance between two coordinates.py
@@ -0,0 +1,11 @@
+def distance(x, y):
+    """
+    distance(x,y) gives the distance between two points: x = [a1, b1] and 
+    y = [a2, b2]
+    """
+    import math
+    diff = math.sqrt((y[0]-x[0])**2 + (y[1]-x[1])**2)
+    print(diff) 
+
+distance([0,0],[0,0])
+    
