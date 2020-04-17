72c7608f501a5660dcdce0cd7972150f424e11e4
Hector Lancaster <56166489+HectorLancaster@users.noreply.github.com>
2020-04-17 10:13:53 +0100

    Add files via upload

---
+++ First Arrays.py
@@ -0,0 +1,18 @@
+
+import numpy as np
+
+x = np.array([1,2,3])
+
+y = np.array([2,4,6])
+
+np.array([[1,3], [5,9]])
+#Out[4]: 
+#array([[1, 3],
+#       [5, 9]])
+
+A = np.array([[1,3], [5,9]])
+
+A.transpose()
+#Out[6]: 
+#array([[1, 5],
+#       [3, 9]])
